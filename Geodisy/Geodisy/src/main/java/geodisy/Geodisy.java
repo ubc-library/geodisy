84b029441cd3f55b9f938e8db27ee9a499b3d83a
Paul Dante <paul.dante@ubc.ca>
2019-01-30 14:31:40 -0800

    Refactoring and adding skeleton of exportToGeoserver
    Added Javadocs comments, refactored method names to be clearer, and added a method to export newly imported data/metadata to geoserver (skeleton of method).

--- Geodisy/Geodisy/src/main/java/geodisy/Geodisy.java
+++ Geodisy/Geodisy/src/main/java/geodisy/Geodisy.java
@@ -7,27 +7,34 @@ package geodisy;
 
 import geodisy.DataSourceLocations.Dataverse;
 import geodisy.Dataverse.DataverseAPI;
+import geodisy.Dataverse.SourceAPI;
 
 
 /**
- *
+ * This is the main activity class of the Geodisy middleware.
+ * From here the calls for harvesting of Dataverse and exporting to Geoserver happen.
  * @author pdante
  */
 class Geodisy {
     public Geodisy() {
     }
     
-    public void getMetadata(){
+    public void harvestDataverse(){
         Dataverse dv = new Dataverse();
         String[] dataverses = dv.getDataverses();
         for(String s: dataverses){
-        DataverseAPI dvAPI = new DataverseAPI(createURL(s));
+        SourceAPI dvAPI = new DataverseAPI(createDataverseURL(s));
         }
     }
-    /*creates the universal part of the dataverse api search/retrieve URLs "://{database name}/api/". 
-    Will still need to add http/http and whatever is needed at the end.*/
-    private String createURL(String s) {
+    /** Creates the universal part of the Dataverse API search/retrieve 
+     * URLs "://{database name}/api/". 
+     * Will still need to add http/http and whatever is needed at the end.*/
+    
+    private String createDataverseURL(String s) {
         String answer = "://" + s + "/api/";
         return answer;
+    }
+    public void exportToGeoserver(){
+        //TODO
     }
 }
