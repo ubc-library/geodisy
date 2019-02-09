ace56a701fd5a994de8e62933715687a820b1ad8
Paul Dante <paul.dante@ubc.ca>
2019-02-08 16:04:01 -0800

    Dataverse Java object constructor and "add" method
    DataverseJavaObject initiates all lists and adds an "add" method for each list.

--- Geodisy/Geodisy/src/main/java/Dataverse/DataverseJavaObject.java
+++ Geodisy/Geodisy/src/main/java/Dataverse/DataverseJavaObject.java
@@ -4,6 +4,8 @@ package Dataverse;
 
 import Dataverse.DataverseJSONFieldClasses.*;
 import Dataverse.DataverseJSONFieldClasses.DataverseJSONGeoFieldClasses.*;
+
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -13,7 +15,7 @@ import java.util.List;
  */
 
 public class DataverseJavaObject {
-    private String title, subtitle, alternativeTitle, alternativeURL;
+    private String title, subtitle, alternativeTitle, alternativeURL, license;
     private List<OtherID> otherID;
     private List<Author> author;
     private List<DatasetContact> datasetContact;
@@ -40,6 +42,32 @@ public class DataverseJavaObject {
     private List<GeographicCoverage> geographicCoverage;
     private List<GeographicBoundingBox> geographicBoundingBox;
 
+    public DataverseJavaObject() {
+        this.otherID = new LinkedList<>();
+        this.author = new LinkedList<>();
+        this.datasetContact = new LinkedList<>();
+        this.dsDescription = new LinkedList<>();
+        this.subject = new LinkedList<>();
+        this.keyword = new LinkedList<>();
+        this.topicClassification = new LinkedList<>();
+        this.publication = new LinkedList<>();
+        this.language = new LinkedList<>();
+        this.producer = new LinkedList<>();
+        this.contributor = new LinkedList<>();
+        this.grantNumber = new LinkedList<>();
+        this.distributor  = new LinkedList<>();
+        this.timePeriodCovered = new LinkedList<>();
+        this.dateOfCollection = new LinkedList<>();
+        this.kindOfData = new LinkedList<>();
+        this.series  = new LinkedList<>();
+        this.software = new LinkedList<>();
+        this.relatedMaterial = new LinkedList<>();
+        this.relatedDatasets = new LinkedList<>();
+        this.otherReferences = new LinkedList<>();
+        this.dataSources = new LinkedList<>();
+        this.geographicCoverage = new LinkedList<>();
+        this.geographicBoundingBox = new LinkedList<>();
+    }
 
     public String getTitle() {
         return title;
@@ -81,6 +109,8 @@ public class DataverseJavaObject {
         this.otherID = otherID;
     }
 
+    public void addOtherID(OtherID oID){this.otherID.add(oID);}
+
     public List<Author> getAuthor() {
         return author;
     }
@@ -88,6 +118,8 @@ public class DataverseJavaObject {
     public void setAuthor(List<Author> author) {
         this.author = author;
     }
+
+    public void addAuthor(Author author){this.author.add(author);}
 
     public List<DatasetContact> getDatasetContact() {
         return datasetContact;
@@ -97,6 +129,8 @@ public class DataverseJavaObject {
         this.datasetContact = datasetContact;
     }
 
+    public void addDatasetContact(DatasetContact dc) {this.datasetContact.add(dc);}
+
     public List<Description> getDsDescription() {
         return dsDescription;
     }
@@ -104,6 +138,8 @@ public class DataverseJavaObject {
     public void setDsDescription(List<Description> dsDescription) {
         this.dsDescription = dsDescription;
     }
+
+    public void addDsDescription(Description d){this.dsDescription.add(d);}
 
     public List<String> getSubject() {
         return subject;
@@ -113,6 +149,8 @@ public class DataverseJavaObject {
         this.subject = subject;
     }
 
+    public void addSubject(String s){this.subject.add(s);}
+
     public List<Keyword> getKeyword() {
         return keyword;
     }
@@ -120,6 +158,8 @@ public class DataverseJavaObject {
     public void setKeyword(List<Keyword> keyword) {
         this.keyword = keyword;
     }
+
+    public void addKeyword(Keyword k){this.keyword.add(k);}
 
     public List<TopicClassification> getTopicClassification() {
         return topicClassification;
@@ -129,6 +169,8 @@ public class DataverseJavaObject {
         this.topicClassification = topicClassification;
     }
 
+    public void addTopicClassification(TopicClassification tc){this.topicClassification.add(tc);}
+
     public List<RelatedPublication> getPublication() {
         return publication;
     }
@@ -136,6 +178,8 @@ public class DataverseJavaObject {
     public void setPublication(List<RelatedPublication> publication) {
         this.publication = publication;
     }
+
+    public void addPublication(RelatedPublication rp){this.publication.add(rp);}
 
     public String getNotesText() {
         return notesText;
@@ -153,6 +197,8 @@ public class DataverseJavaObject {
         this.language = language;
     }
 
+    public void addLanguage(String s){this.language.add(s);}
+
     public List<Producer> getProducer() {
         return producer;
     }
@@ -160,6 +206,8 @@ public class DataverseJavaObject {
     public void setProducer(List<Producer> producer) {
         this.producer = producer;
     }
+
+    public void addProducer(Producer p){this.producer.add(p);}
 
     public String getProductionDate() {
         return productionDate;
@@ -185,6 +233,8 @@ public class DataverseJavaObject {
         this.contributor = contributor;
     }
 
+    public void addContributor(Contributor c){this.contributor.add(c);}
+
     public List<GrantNumber> getGrantNumber() {
         return grantNumber;
     }
@@ -193,6 +243,8 @@ public class DataverseJavaObject {
         this.grantNumber = grantNumber;
     }
 
+    public void addGrantNumber(GrantNumber gn){this.grantNumber.add(gn);}
+
     public List<Distributor> getDistributor() {
         return distributor;
     }
@@ -200,6 +252,8 @@ public class DataverseJavaObject {
     public void setDistributor(List<Distributor> distributor) {
         this.distributor = distributor;
     }
+
+    public void addDistributor(Distributor d){this.distributor.add(d);}
 
     public String getDistributionDate() {
         return distributionDate;
@@ -233,6 +287,8 @@ public class DataverseJavaObject {
         this.timePeriodCovered = timePeriodCovered;
     }
 
+    public void addTimePeriodCovered(TimePeriodCovered tpc){this.timePeriodCovered.add(tpc);}
+
     public List<DateOfCollection> getDateOfCollection() {
         return dateOfCollection;
     }
@@ -240,6 +296,8 @@ public class DataverseJavaObject {
     public void setDateOfCollection(List<DateOfCollection> dateOfCollection) {
         this.dateOfCollection = dateOfCollection;
     }
+
+    public void addDateOfCollection(DateOfCollection dc){this.dateOfCollection.add(dc);}
 
     public List<String> getKindOfData() {
         return kindOfData;
@@ -249,6 +307,8 @@ public class DataverseJavaObject {
         this.kindOfData = kindOfData;
     }
 
+    public void addKindOfData(String s){this.kindOfData.add(s);}
+
     public List<Series> getSeries() {
         return series;
     }
@@ -256,6 +316,8 @@ public class DataverseJavaObject {
     public void setSeries(List<Series> series) {
         this.series = series;
     }
+
+    public void addSeries(Series s){this.series.add(s);}
 
     public List<Software> getSoftware() {
         return software;
@@ -265,6 +327,8 @@ public class DataverseJavaObject {
         this.software = software;
     }
 
+    public void addSoftware(Software s){this.software.add(s);}
+
     public List<String> getRelatedMaterial() {
         return relatedMaterial;
     }
@@ -272,6 +336,8 @@ public class DataverseJavaObject {
     public void setRelatedMaterial(List<String> relatedMaterial) {
         this.relatedMaterial = relatedMaterial;
     }
+
+    public void addRelatedMaterial(String s){this.relatedMaterial.add(s);}
 
     public List<String> getRelatedDatasets() {
         return relatedDatasets;
@@ -281,6 +347,8 @@ public class DataverseJavaObject {
         this.relatedDatasets = relatedDatasets;
     }
 
+    public void addRelatedDatasets(String s){this.relatedDatasets.add(s);}
+
     public List<String> getOtherReferences() {
         return otherReferences;
     }
@@ -289,6 +357,8 @@ public class DataverseJavaObject {
         this.otherReferences = otherReferences;
     }
 
+    public void addOtherReference(String s){this.otherReferences.add(s);}
+
     public List<String> getDataSources() {
         return dataSources;
     }
@@ -296,6 +366,8 @@ public class DataverseJavaObject {
     public void setDataSources(List<String> dataSources) {
         this.dataSources = dataSources;
     }
+
+    public void addDataSource(String s){this.dataSources.add(s);}
 
     public String getOriginOfSources() {
         return originOfSources;
@@ -329,11 +401,23 @@ public class DataverseJavaObject {
         this.geographicCoverage = geographicCoverage;
     }
 
+    public void addGeographicCoverage(GeographicCoverage gc){this.geographicCoverage.add(gc);}
+
     public List<GeographicBoundingBox> getGeographicBoundingBox() {
         return geographicBoundingBox;
     }
 
     public void setGeographicBoundingBox(List<GeographicBoundingBox> geographicBoundingBox) {
         this.geographicBoundingBox = geographicBoundingBox;
+    }
+
+    public void addGeographicBoundingBox(GeographicBoundingBox gbb){this.geographicBoundingBox.add(gbb);}
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
     }
 }
