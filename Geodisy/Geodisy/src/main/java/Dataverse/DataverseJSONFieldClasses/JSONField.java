1160c5eec3bb4b2846832ddb3e8948df9ccb4f50
Paul Dante <paul.dante@ubc.ca>
2019-02-22 15:10:18 -0800

    Fixed URL Parsing with Apache Commons URLValidator
    Using Apache Commons to validate URL structure rather than REGEX

--- Geodisy/Geodisy/src/main/java/Dataverse/DataverseJSONFieldClasses/JSONField.java
+++ Geodisy/Geodisy/src/main/java/Dataverse/DataverseJSONFieldClasses/JSONField.java
@@ -1,9 +1,10 @@
 package Dataverse.DataverseJSONFieldClasses;
 
 import Crosswalking.JSONParsing.DataverseParser;
+
+import org.apache.commons.validator.routines.UrlValidator;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.json.JSONObject;
 
 public abstract class JSONField {
     protected Logger logger = LogManager.getLogger(DataverseParser.class);
@@ -13,8 +14,10 @@ public abstract class JSONField {
      * @return Either a validly formated URL or an empty String
      */
     protected String filterURL(String value) {
-        String URLFILTER = "^((?i)((http|https):\\/\\/(www))|(www))?.(?i)([\\w]+\\.)+(\\/[\\w\\/]*)*\\??([\\&a-z1-9=]*)?";
-        if(value.matches(URLFILTER))
+        //String URLFILTER = "^((?i)((http|https):\\/\\/(www))|(www))?.(?i)([\\w]+\\.)+(\\/[\\w\\/]*)*\\??([\\&a-z1-9=]*)?";
+        UrlValidator urlValidator = new UrlValidator();
+
+        if(urlValidator.isValid(value))
             return value;
         logger.error("Malformed URL error (%s), returning blank String", value);
         return "";
