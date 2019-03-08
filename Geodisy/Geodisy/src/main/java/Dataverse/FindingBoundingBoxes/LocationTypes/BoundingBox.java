90313d45a853cff7dca4c4ed5a726689c6234eed
Paul Dante <paul.dante@ubc.ca>
2019-03-07 17:03:35 -0800

    BoundingBox class can now deal with either double or String vals
    When values are put into BoundingBox, doubles and Strings are parsed for validity.

--- Geodisy/Geodisy/src/main/java/Dataverse/FindingBoundingBoxes/LocationTypes/BoundingBox.java
+++ Geodisy/Geodisy/src/main/java/Dataverse/FindingBoundingBoxes/LocationTypes/BoundingBox.java
@@ -10,12 +10,60 @@ public class BoundingBox {
     public BoundingBox() {
     }
 
+    /**
+     *
+     * @param lat or long
+     * @return 361 if value is outside the range for lat or long, respectively, or returns val if valid
+     */
+
+    private double checkLat(double lat) {
+        if(lat<-90 || lat>90)
+            return 361;
+        return lat;
+    }
+
+    private double checkLong(double longitude) {
+        if(longitude<-180 || longitude>180)
+            return 361;
+        return longitude;
+    }
+
+    /**
+     *
+     * @param numString
+     * @return Double parsed from the given string or 361 if the string was not parsable into a double
+     */
+    private double getDoubleLat(String numString) {
+        double val;
+        try{
+            val = Double.parseDouble(numString);
+        } catch (NumberFormatException e){
+            val = 361;
+        }
+        return checkLat(val);
+    }
+
+    private double getDoubleLong(String numString) {
+        double val;
+        try{
+            val = Double.parseDouble(numString);
+        } catch (NumberFormatException e){
+            val = 361;
+        }
+        return checkLong(val);
+    }
+
     public double getLatSouth() {
         return latSouth;
     }
 
     public void setLatSouth(double latSouth) {
-        this.latSouth = latSouth;
+        this.latSouth = checkLat(latSouth);
+
+    }
+
+    public void setLatSouth(String latSouth){
+        this.latSouth = getDoubleLat(latSouth);
     }
 
     public double getLatNorth() {
@@ -23,7 +71,11 @@ public class BoundingBox {
     }
 
     public void setLatNorth(double latNorth) {
-        this.latNorth = latNorth;
+        this.latNorth = checkLat(latNorth);
+    }
+
+    public void setLatNorth(String latNorth){
+        this.latNorth = getDoubleLat(latNorth);
     }
 
     public double getLongWest() {
@@ -31,7 +83,11 @@ public class BoundingBox {
     }
 
     public void setLongWest(double longWest) {
-        this.longWest = longWest;
+        this.longWest = checkLong(longWest);
+    }
+    
+    public void setLongWest(String longWest){
+        this.longWest =  getDoubleLong(longWest);
     }
 
     public double getLongEast() {
@@ -41,4 +97,10 @@ public class BoundingBox {
     public void setLongEast(double longEast) {
         this.longEast = longEast;
     }
+
+    public void setLongEast(String longEast){
+        this.longEast = getDoubleLong(longEast);
+    }
+
+
 }
