4a743d5ccce5ce77708381fb9b1020da015f0cd0
Paul Dante <paul.dante@ubc.ca>
2019-03-25 11:37:57 -0700

    Added try/catch to parse() and parse or create Geo metadata fields
    If metadata has a geospatial section, parse it, otherwise create a new empty geofields class to attach to the DJO.

--- Geodisy/Geodisy/src/main/java/Crosswalking/JSONParsing/DataverseParser.java
+++ Geodisy/Geodisy/src/main/java/Crosswalking/JSONParsing/DataverseParser.java
@@ -16,7 +16,7 @@ public class DataverseParser {
     private JSONObject dataverseJSON;
     private DataverseJavaObject dJO;
     static Logger logger = LogManager.getLogger(DataverseParser.class);
-    //TODO compartimentalize Citation metadata, Geographic Metadata, etc
+
     public DataverseParser(String dataverseJSONString) {
         this.dataverseJSON = new JSONObject(dataverseJSONString);
         this.dJO = new DataverseJavaObject();
@@ -28,21 +28,27 @@ public class DataverseParser {
      * to parse compound or simple field data, respectively.
      * @throws JSONException
      */
-    private void parse() throws JSONException {
-        JSONObject current = (JSONObject) dataverseJSON.get("data");
-        dJO.parseCitationFields(current);
-        JSONObject metadata = current.getJSONObject("latestVersion").getJSONObject("metadataBlocks");
-        if(metadata.has(GEOSPATIAL))
-            dJO.parseGeospatialFields(metadata.getJSONObject(GEOSPATIAL).getJSONArray(FIELDS));
-        String prodPlace = dJO.getProductionPlace();
-        if(!prodPlace.matches("")&&!dJO.hasBoundingBox()){
-            GeographicFields gf = dJO.getGeographicFields();
-            gf.setFullBB(getBBFromProdPlace(prodPlace));
+    private void parse()  {
+        try {
+            JSONObject current = (JSONObject) dataverseJSON.get("data");
+            dJO.parseCitationFields(current);
+            JSONObject metadata = current.getJSONObject("latestVersion").getJSONObject("metadataBlocks");
+            if (metadata.has(GEOSPATIAL))
+                dJO.parseGeospatialFields(metadata.getJSONObject(GEOSPATIAL).getJSONArray(FIELDS));
+            else
+                dJO.setGeoFields(new GeographicFields());
+            String prodPlace = dJO.getProductionPlace();
+            if (!prodPlace.matches("") && !dJO.hasBoundingBox()) {
+                GeographicFields gf = dJO.getGeographicFields();
+                gf.setFullBB(getBBFromProdPlace(prodPlace));
+            }
+        }catch (JSONException e){
+            logger.error("Something was malformed with the JSON string returned from Dataverse");
         }
-
     }
 
-    //TODO get bounding box from production name using geonames, if possible
+    // We decided that we aren't trying to get a bounding box from Production Place but will instead log it to
+    // the manual check log
     private BoundingBox getBBFromProdPlace(String prodPlace) {
         BoundingBox b = new BoundingBox();
         logger.info("The following record has no geographic location info other than a Production Place. Please manually check: %s", dJO.getDOI());
