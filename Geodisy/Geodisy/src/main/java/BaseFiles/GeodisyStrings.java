b6b82eea0a2a3ad1eec90ff07ae95ab77ec301f3
Paul_Laptop <paul.dante@ubc.ca>
2019-04-16 17:59:16 -0700

    Add a string for the error file, but still need to log things there

--- Geodisy/Geodisy/src/main/java/BaseFiles/GeodisyStrings.java
+++ Geodisy/Geodisy/src/main/java/BaseFiles/GeodisyStrings.java
@@ -2,7 +2,9 @@ package BaseFiles;
 
 public class GeodisyStrings {
 
+    //TODO log errors to the correct file
     //File paths
     public final static String EXISTING_RECORDS = "./savedFiles/ExisitingRecords.txt";
     public final static String RECORDS_TO_CHECK = "./logs/recordsToCheck.log";
+    public final static String ERROR_LOG = "./logs/error.log";
 }
