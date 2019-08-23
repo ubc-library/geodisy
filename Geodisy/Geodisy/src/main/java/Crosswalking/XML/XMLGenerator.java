06d0d7f247066a0f28d6f34c5713a3b2cc2db923
Paul_Laptop <paul.dante@ubc.ca>
2019-08-23 10:16:46 -0700

    Removed duplicate/unused methods. Added some comments to code

--- Geodisy/Geodisy/src/main/java/Crosswalking/XML/XMLGenerator.java
+++ Geodisy/Geodisy/src/main/java/Crosswalking/XML/XMLGenerator.java
@@ -37,10 +37,14 @@ public class XMLGenerator {
         Element rootElement = getRoot();
         SubElement ident = new IdentificationInfo(djo, doc, rootElement);
         rootElement = ident.getFields();
+
+        //DataQuality Fields
         if(simple.hasField(ACCESS_TO_SOURCES)||simple.hasField(ORIG_OF_SOURCES)||simple.hasField(CHAR_OF_SOURCES)||citationFields.getListField(DATA_SOURCES).size()>0) {
             SubElement dqi = new DataQualityInfo(djo, doc, rootElement);
             rootElement.appendChild(dqi.getFields());
         }
+
+        //Distributor Fields
         List distributors = citationFields.getListField(DISTRIB);
         if(distributors.size()>0||simple.hasField(DEPOSITOR)) {
             SubElement distribution = new DistributionInfo(djo,doc,rootElement,simple, distributors);
@@ -53,9 +57,7 @@ public class XMLGenerator {
         doc.addRoot(rootElement);
         return doc;
     }
-
-
-
+    
     private Element getRoot() {
         // root element
         Element rootElement = doc.createGMDElement("MD_Metadata");
@@ -68,46 +70,4 @@ public class XMLGenerator {
         rootElement.setAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
         return rootElement;
     }
-
-
-
-
-
-    //TODO need more input from Mark
-    private Element getIdentifier(Element ci_citation) {
-        LinkedList otherIDs = (LinkedList) citationFields.getOtherIDs();
-
-
-        return ci_citation;
-    }
-
-
-
-    private Element roleCode(String val){
-        Element role = doc.createGMDElement("role");
-        Element ci = doc.createGMDElement("CI_RoleCode");
-        ci.setTextContent(val);
-        role.appendChild(ci);
-        return role;
-    }
-
-    //TODO add whatever other fields are possibly needed for descriptive keywords
-    private Element descriptiveKeywards(LinkedList<String> keywords, String type){
-        return descriptiveKeywards(keywords, type, "nilReason=unknown");
-    }
-
-    private Element descriptiveKeywards(LinkedList<String> keywords, String type, String thesaurus){
-        Element descriptiveK = doc.createGMDElement("descriptiveKeywords");
-        for(String s: keywords){
-            Element keyword = doc.createGMDElement("keyword");
-            Element charString = doc.addGCOVal(s,CHARACTER);
-            keyword.appendChild(charString);
-            descriptiveK.appendChild(keyword);
-        }
-        Element typeEl = doc.createGMDElement("type");
-        return descriptiveK;
-    }
-
-
-    
 }
