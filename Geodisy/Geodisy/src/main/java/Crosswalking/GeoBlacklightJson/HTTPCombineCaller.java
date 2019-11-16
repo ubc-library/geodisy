5571d92df07c8ea46fb2554c83c3a6fcb2f933ef
Paul_Laptop <paul.dante@ubc.ca>
2019-11-15 22:09:36 -0800

    Added constructor to GeoCombine HTTPCaller

--- Geodisy/Geodisy/src/main/java/Crosswalking/GeoBlacklightJson/HTTPCombineCaller.java
+++ Geodisy/Geodisy/src/main/java/Crosswalking/GeoBlacklightJson/HTTPCombineCaller.java
@@ -1,8 +1,13 @@
 package Crosswalking.GeoBlacklightJson;
 
+import BaseFiles.GeoLogger;
 import BaseFiles.HTTPCaller;
 
 public class HTTPCombineCaller extends HTTPCaller {
+
+    public HTTPCombineCaller() {
+        logger = new GeoLogger(this.getClass());
+    }
 
     @Override
     protected void ioError() {
