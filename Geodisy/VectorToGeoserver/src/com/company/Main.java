387b3056dd3d8be83496fbea58e9163eacae4807
Paul_Laptop <paul.dante@ubc.ca>
2020-01-16 22:13:31 -0800

    Program to bring records from POSTGIS to Geoserver

--- Geodisy/VectorToGeoserver/src/com/company/Main.java
+++ Geodisy/VectorToGeoserver/src/com/company/Main.java
@@ -7,8 +7,8 @@ public class Main {
 
     public static void main(String[] args) {
         String geoserverLabel = "10_5072_fk2_vwschav1";
-        String deleteFirst = "curl -v -u admin:geoserver -X DELETE \"http://localhost:8080/geoserver/rest/workspaces/geodisy/" + geoserverLabel.toLowerCase() + "\"?recurse=true -H  \"accept: application/json\" -H  \"content-type: application/json\"";
-        String call = "curl -v -u admin:geoserver -XPOST -H \"Content-type: text/xml\" -d \"<featureType><name>" + geoserverLabel.toLowerCase() + "</name><nativeCRS>EPSG:4326</nativeCRS><srs>EPSG:4326</srs><enabled>true</enabled></featureType>\" http://localhost:8080/geoserver/rest/workspaces/geodisy/datastores/" + "testvectordata" + "/featuretypes";
+        String deleteFirst = "curl -v -u admin:geoserver -X DELETE \"http://localhost:8080/geoserver/rest/workspaces/geodisy/doi" + geoserverLabel.toLowerCase() + "\"?recurse=true -H  \"accept: application/json\" -H  \"content-type: application/json\"";
+        String call = "curl -v -u admin:geoserver -XPOST -H \"Content-type: text/xml\" -d \"<featureType><name>doi" + geoserverLabel.toLowerCase() + "</name><nativeCRS>EPSG:4326</nativeCRS><srs>EPSG:4326</srs><enabled>true</enabled></featureType>\" http://localhost:8080/geoserver/rest/workspaces/geodisy/datastores/" + "testvectordata" + "/featuretypes";
         ProcessBuilder processBuilder= new ProcessBuilder("bash", "-c", deleteFirst);
         Process process;
         InputStream inputStream;
