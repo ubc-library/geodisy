0127beed3e2fa2ba5630d23097531867832f2cd0
Paul_Laptop <paul.dante@ubc.ca>
2020-02-13 17:03:30 -0800

    Inconsequential changes

--- Geodisy/Geodisy/src/main/java/Dataverse/FindingBoundingBoxes/Countries.java
+++ Geodisy/Geodisy/src/main/java/Dataverse/FindingBoundingBoxes/Countries.java
@@ -8,7 +8,6 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -16,7 +15,6 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.StringReader;
 import java.util.HashMap;
 import org.apache.commons.text.WordUtils;
 
