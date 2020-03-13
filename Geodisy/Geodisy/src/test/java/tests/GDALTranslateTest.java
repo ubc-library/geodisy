e19a69644304dfd5c4b3fe1cca6617f1de34cd56
Paul_Laptop <paul.dante@ubc.ca>
2020-03-13 12:34:22 -0700

    More test stuff

--- Geodisy/Geodisy/src/test/java/tests/GDALTranslateTest.java
+++ Geodisy/Geodisy/src/test/java/tests/GDALTranslateTest.java
@@ -2,14 +2,34 @@ package tests;
 
 import Dataverse.GDAL;
 import Dataverse.GDALTranslate;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 import static BaseFiles.GeodisyStrings.GDAL_TRANSLATE;
 import static BaseFiles.GeodisyStrings.OGR2OGR;
 
 public class GDALTranslateTest {
+    @BeforeEach
+    void setup(){
+        String[] exts = {".prj",".sbn",".shp",".shx"};
+        String source = "src/test/UnzipTestDepositLocation/OldStreamsPolyline - Copy";
+        String destPath = "src/test/UnzipTestDepositLocation/OldStreamsPolyline ";
+        for(String s: exts){
+            File sourceFile = new File(source+s);
+            File destFile = new File(destPath+s);
+            try {
+                Files.copy(sourceFile.toPath(),destFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
     @Test
     public void utm2LatLongTest(){
         String baseCall = OGR2OGR;
