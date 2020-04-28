13d8356170f3c7b84293b2e6eae1650cd5ab9163
Paul <maps1984@hotmail.com>
2020-04-28 00:55:24 -0700

    No longer named testVectordata DBs

--- Geodisy/Geodisy/src/main/java/GeoServer/GeoserverStrings.java
+++ Geodisy/Geodisy/src/main/java/GeoServer/GeoserverStrings.java
@@ -1,15 +1,12 @@
 package GeoServer;
 
-import static BaseFiles.GeodisyStrings.TEST;
 import static BaseFiles.GeodisyStrings.getRoot;
 
 public class GeoserverStrings {
 
     static String WORKSPACE_NAME = "geodisy";
     public static String POSTGIS_BD = "";
-    static String PROD_VECTOR_DB = "vectordata";
-    static String TEST_VECTOR_DB = "testvectordata";
-    static final String VECTOR_DB = getVectorDB();
+    public static String VECTOR_DB = "vectordata";
     static String LOCATION = "tbd";
     static String SHP_2_PGSQL = "/usr/pgsql-12/bin/shp2pgsql -d " + getRoot() + "datasetFiles/";
     static String PSQL_CALL = "| /usr/pgsql-12/bin/psql -d ";
@@ -17,10 +14,4 @@ public class GeoserverStrings {
     static String POSTGIS_USER_CALL = " -U " + POSTGIS_USER;
     public static String POSTGRES_SCHEMA = " public.";
 
-    public static String getVectorDB(){
-        if(TEST)
-            return TEST_VECTOR_DB;
-        else
-            return PROD_VECTOR_DB;
-    }
 }
