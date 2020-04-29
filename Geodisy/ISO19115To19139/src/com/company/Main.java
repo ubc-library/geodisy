db17a8b2669914b96c46c01530e1d7a1286690a3
Paul <maps1984@hotmail.com>
2020-04-28 23:05:48 -0700

    Added description to ISO19115ToISO19139

--- Geodisy/ISO19115To19139/src/com/company/Main.java
+++ Geodisy/ISO19115To19139/src/com/company/Main.java
@@ -3,7 +3,8 @@ package com.company;
 import java.io.File;
 
 public class Main {
-
+	//Convert geoblacklight.json, ISO19115.xml, and ISO19115.zip to ISO19139
+	//Add jar to folder with all the metadata and run. It will recurse through all the folders
     public static void main(String[] args) {
     	System.out.println("Got here");
 		File folder = new File(System.getProperty("user.dir"));
