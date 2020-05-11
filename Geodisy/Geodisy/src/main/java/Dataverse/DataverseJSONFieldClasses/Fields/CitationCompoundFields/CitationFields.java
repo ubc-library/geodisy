567120f0bf2e61d0ae785e223a1719ccae0233bd
Paul <maps1984@hotmail.com>
2020-05-10 21:02:47 -0700

    Adding last modified date to GeoBlacklight records

--- Geodisy/Geodisy/src/main/java/Dataverse/DataverseJSONFieldClasses/Fields/CitationCompoundFields/CitationFields.java
+++ Geodisy/Geodisy/src/main/java/Dataverse/DataverseJSONFieldClasses/Fields/CitationCompoundFields/CitationFields.java
@@ -241,6 +241,7 @@ public class CitationFields extends MetadataSimple {
         simpleCitationFields.setField(LICENSE,parseSimpleValue(current,LICENSE));
         simpleCitationFields.setField(MAJOR_VERSION,parseSimpleValue(current,MAJOR_VERSION));
         simpleCitationFields.setField(MINOR_VERSION,parseSimpleValue(current,MINOR_VERSION));
+        simpleCitationFields.setField(LAST_MOD_DATE,parseSimpleValue(current,LAST_MOD_DATE));
         getTOSAndAccessFields(current);
 
     }
