bd776abd696bd8b32739602c2ebc4ebaaafd58fb
Paul <maps1984@hotmail.com>
2020-05-27 13:34:34 -0700

    Added a RasterUpload test option to run from main

--- Geodisy/Geodisy/src/main/java/BaseFiles/Main.java
+++ Geodisy/Geodisy/src/main/java/BaseFiles/Main.java
@@ -5,7 +5,15 @@ package BaseFiles;
  * and open the template in the editor.
  */
 
-import static _Strings.GeodisyStrings.TEST;
+import Dataverse.DataverseGeoRecordFile;
+import Dataverse.DataverseJSONFieldClasses.Fields.CitationCompoundFields.CitationFields;
+import Dataverse.DataverseJSONFieldClasses.Fields.CitationSimpleJSONFields.SimpleCitationFields;
+import Dataverse.DataverseJavaObject;
+import Dataverse.SourceJavaObject;
+import GeoServer.GeoServerAPI;
+
+import static _Strings.DVFieldNameStrings.RECORD_URL;
+import static _Strings.GeodisyStrings.*;
 
 /**
  *
@@ -31,9 +39,11 @@ public class Main {
         else
             dev = "Using the prod servers, is this correct?";
         System.out.println(dev);
-        GeodisyTask geodisyTask = new GeodisyTask();
+       GeodisyTask geodisyTask = new GeodisyTask();
         geodisyTask.run();
 
+        /*System.out.println("Running a test on Raster Upload");
+        testRasterUploadToGeoserver();*/
         /*GeoCombine geoCombine = new GeoCombine();
         geoCombine.index();*/
 
@@ -43,5 +53,22 @@ public class Main {
 
 
     }
-    
+
+    private static void testRasterUploadToGeoserver() {
+        SourceJavaObject sjo = new DataverseJavaObject(SCHOLARS_PORTAL);
+        CitationFields cf = sjo.getCitationFields();
+        SimpleCitationFields sf = cf.getSimpleCitationFields();
+        sf.setField(RECORD_URL,"10.5683/SP2/UEJGTV");
+        cf.setSimpleCitationFields(sf);
+        sjo.setCitationFields(cf);
+        DataverseGeoRecordFile dgrf = new DataverseGeoRecordFile();
+        dgrf.setOriginalTitle("VanOldStreamOriginalMapScan.zip");
+        dgrf.setTranslatedTitle("VanOldStreamOriginalMapScan.tif");
+        dgrf.setGeometryType(RASTER);
+        dgrf.setGeoserverLabel("g_10_5683_SP2_UEJGTV");
+        dgrf.setIsFromFile(true);
+        GeoServerAPI geo = new GeoServerAPI(sjo);
+        geo.addRaster(dgrf);
+    }
+
 }
