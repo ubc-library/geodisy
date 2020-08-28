6224eb761de890112b010fdfade0f10a011ec603
Paul <paul.dante@ubc.ca>
2020-08-28 03:41:15 -0700

    Added sudo to shpToPgsql call

--- Geodisy/Geodisy/src/main/java/_Strings/GeoserverStrings.java
+++ Geodisy/Geodisy/src/main/java/_Strings/GeoserverStrings.java
@@ -8,10 +8,10 @@ public class GeoserverStrings {
     public static String VECTOR_DB = "vectordata";
     public static String GEOSERVER_VECTOR_STORE = "vectordata";
     public static String LOCATION = "tbd";
-    public static String SHP_2_PGSQL = "/usr/pgsql-12/bin/shp2pgsql -d " + DATA_DIR_LOC;
+    public static String SHP_2_PGSQL = "sudo /usr/pgsql-12/bin/shp2pgsql -d " + DATA_DIR_LOC;
     public static String PSQL_CALL = "| PGPASSWORD=" + POSTGIS_USER_PASSWORD +" /usr/pgsql-12/bin/psql -d ";
     public static String POSTGIS_USER = "geodisy_user";
     public static String POSTGIS_USER_CALL = " -U " + POSTGIS_USER;
-    public static String POSTGRES_SCHEMA = " public.";
+    public static String POSTGRES_SCHEMA = "public.";
 
 }
