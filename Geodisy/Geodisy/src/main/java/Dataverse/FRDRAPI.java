e02c5ba2ebf2122565a5f17f47fa47491a3d335d
Paul Dante <paul.dante@ubc.ca>
2020-11-25 06:09:27 -0800

    Created API for FRDR Harvester usage. Need API endpoints

---
+++ Geodisy/Geodisy/src/main/java/Dataverse/FRDRAPI.java
@@ -0,0 +1,46 @@
+package Dataverse;
+
+import Crosswalking.JSONParsing.DataverseParser;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.util.LinkedList;
+
+public class FRDRAPI{
+    public LinkedList<DataverseJavaObject> callFRDRHarvester(){
+        boolean done = true;
+        String fullJSON = getJson();
+        JSONParser jsonParser = new JSONParser();
+        JSONObject json = new JSONObject();
+        LinkedList<DataverseJavaObject> djos = new LinkedList<>();
+        try {
+            json = (JSONObject) jsonParser.parse(fullJSON);
+            done = json.getBoolean("finished");
+            JSONArray records = json.getJSONArray("records");
+            DataverseParser parser = new DataverseParser();
+            for (Object o: records){
+                JSONObject jo = (JSONObject) o;
+                djos.add(parser.frdrParse(jo));
+                int record_id = jo.getInt("id");
+                updateFRDRHarvesterDB(record_id);
+            }
+            if(!done) {
+                djos.addAll(callFRDRHarvester());
+            }
+        } catch (ParseException e) {
+            System.out.println("Something went wrong parsing the FRDR JSON");
+        }
+        return djos;
+    }
+
+    private void updateFRDRHarvesterDB(int record_id) {
+        //TODO set this up once Joel has given me the API endpoint to hit
+    }
+
+    private String getJson() {
+        //TODO set this up once Joel has given me the API endpoint to hit
+        return "";
+    }
+}
