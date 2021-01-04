b191b814830293d782ade2741ed022e4188510a7
Paul Dante <paul.dante@ubc.ca>
2021-01-03 18:26:21 -0800

    Updated Geodisy metadata source to FRDR Harvester need to check file DL

--- Geodisy/Geodisy/src/main/java/Dataverse/FRDRAPI.java
+++ Geodisy/Geodisy/src/main/java/Dataverse/FRDRAPI.java
@@ -2,59 +2,158 @@ package Dataverse;
 
 import BaseFiles.GeoLogger;
 import Crosswalking.JSONParsing.DataverseParser;
+import org.json.JSONException;
+import org.json.JSONTokener;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
+import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Calendar;
+import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 
-public class FRDRAPI{
+public class FRDRAPI extends SourceAPI{
     GeoLogger logger =  new GeoLogger(this.getClass());
+
+    // Calls "callFRDRHarvester as prod version rather than testing version so that downloading of files
+    // happens and geoserver is updated
     public LinkedList<SourceJavaObject> callFRDRHarvester(){
+        return callFRDRHarvester(false);
+    }
+
+    public LinkedList<SourceJavaObject> callFRDRHarvester(boolean testing){
         String fullJSON = getJson();
-        JSONParser jsonParser = new JSONParser();
         LinkedList<SourceJavaObject> djos = new LinkedList<>();
         try {
-            JSONObject json = (JSONObject) jsonParser.parse(fullJSON);
-            boolean done = json.getBoolean("finished");
+            JSONTokener tokener = new JSONTokener(fullJSON);
+            JSONObject json = new JSONObject(tokener);
+            boolean done = (boolean)json.get("finished");
             JSONArray records = json.getJSONArray("records");
             DataverseParser parser = new DataverseParser();
             for (Object o: records){
                 JSONObject jo = (JSONObject) o;
-                djos.add(parser.frdrParse(jo));
+                DataverseJavaObject djo = parser.frdrParse(jo);
+                if(djo.hasGeoGraphicCoverage())
+                    djo = (DataverseJavaObject) getBBFromGeonames(djo);
+                if(djo.hasBoundingBox())
+                    djos.add(parser.frdrParse(jo));
                 int record_id = jo.getInt("id");
                 updateFRDRHarvesterDB(record_id);
+                if(djo.hasContent && !testing) {
+                    System.out.println("Downloading record: " + djo.getPID());
+                    long startTime = Calendar.getInstance().getTimeInMillis();
+                    djo = djo.downloadFiles();
+                    Calendar end = Calendar.getInstance();
+                    Long total = end.getTimeInMillis() - startTime;
+                    System.out.println("Finished downloading " + djo.getPID() + " after " + total + " milliseconds");
+                    djo.updateRecordFileNumbers();
+                    djo.updateGeoserver();
+                }
             }
             if(!done) {
-                djos.addAll(callFRDRHarvester());
+                djos.addAll(callFRDRHarvester(testing));
             }
-        } catch (ParseException e) {
+        } catch (JSONException e) {
             System.out.println("Something went wrong parsing the FRDR JSON");
             logger.error("Something went wrong parsing the FRDR JSON: \n" + fullJSON);
         }
         return djos;
     }
 
-    private void updateFRDRHarvesterDB(int record_id) {
+    public void updateFRDRHarvesterDB(int record_id) {
         try {
             //TODO update url with actual prod url
-            String generateWorkspace = "/usr/bin/curl -X PUT -H \"Content-Type: application/json\" -d '{\"geodisy_harvested\":1}' http://test-web-1.frdr.ca:8101/records/" + record_id;
-            ProcessBuilder processBuilder= new ProcessBuilder();
+            //String generateWorkspace = "/usr/bin/curl -X PUT -H \"Content-Type: application/json\" -d '{\"geodisy_harvested\":1}' https://dev3.frdr.ca/harvestapi/record/" + record_id;
+            URL url = new URL("https://dev3.frdr.ca/harvestapi/records/" + record_id);
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            conn.setRequestMethod("PUT");
+            conn.setRequestProperty("Content-Type","application/json; charset=UTF-8");
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            String json = "{\"geodisy_harvested\":1}";
+            OutputStream os = conn.getOutputStream();
+            os.write(json.getBytes("UTF-8"));
+            os.close();
+            InputStream in = new BufferedInputStream(conn.getInputStream());
+            String result = org.apache.commons.io.IOUtils.toString(in, "UTF-8");
+            JSONObject jsonObject = new JSONObject(result);
+
+
+            in.close();
+            conn.disconnect();
+
+            /*ProcessBuilder processBuilder= new ProcessBuilder();
             Process p;
             processBuilder.command("/usr/bin/bash", "-c", generateWorkspace);
             p = processBuilder.start();
             p.waitFor();
-            p.destroy();
-        } catch (InterruptedException| IOException e) {
+            p.destroy();*/
+        } catch (IOException e) {
             logger.error("Something went wrong trying to call FRDR to mark record #" + record_id + " as processed by Geodisy");
         }
     }
 
-    private String getJson() {
+    public String getJson() {
         //TODO set this up once Joel has given me the API endpoint to hit
+        try {
+            URL url = new URL("https://dev3.frdr.ca/harvestapi/exporter");
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.connect();
+            int responsecode = conn.getResponseCode();
+            String inlines = "";
+            if(responsecode != 200)
+                throw new RuntimeException("HttpResponseCode:" +responsecode);
+            else{
+                Scanner sc = new Scanner(url.openStream());
+                while (sc.hasNext()){
+                    inlines+=sc.nextLine();
+                }
+                sc.close();
+                return inlines;
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return "";
+    }
+
+
+    //___________________________________________________________________________________________
+    //No longer needed since metadata coming from FRDR Harvester
+
+    @Override
+    protected HashSet<String> searchDV() {
+        return null;
+    }
+
+    @Override
+    protected LinkedList<JSONObject> downloadMetadata(HashSet<String> dIOs) {
+        return null;
+    }
+
+    @Override
+    public LinkedList<SourceJavaObject> harvest(LinkedList<SourceJavaObject> answers) {
+        return null;
+    }
+
+    @Override
+    protected void deleteMetadata(String identifier) {
+
+    }
+
+    @Override
+    protected void deleteFromGeoserver(String identifier) {
+
     }
 }
---
+++ Geodisy/Geodisy/src/test/java/tests/TestFRDRAPI.java
@@ -0,0 +1,20 @@
+package tests;
+
+import DataSourceLocations.Dataverse;
+import Dataverse.FRDRAPI;
+import Dataverse.SourceJavaObject;
+import org.junit.Test;
+
+import java.util.LinkedList;
+
+public class TestFRDRAPI {
+    @Test
+    public void getJSONRecords(){
+        FRDRAPI f = new FRDRAPI();
+        LinkedList<SourceJavaObject> djos = f.callFRDRHarvester(true);
+        for(SourceJavaObject s: djos){
+            System.out.println(s.getPID());
+        }
+
+    }
+}
