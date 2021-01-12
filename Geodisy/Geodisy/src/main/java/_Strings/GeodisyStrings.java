b59f3e434e911260da2210f5e1fcf522d11abe95
Paul Dante <paul.dante@ubc.ca>
2021-01-12 12:58:41 -0800

    Why isn't replace working

--- Geodisy/Geodisy/src/main/java/_Strings/GeodisyStrings.java
+++ Geodisy/Geodisy/src/main/java/_Strings/GeodisyStrings.java
@@ -333,7 +333,7 @@ public class GeodisyStrings {
 
     public static String removeHTTPS(String path) {
         path = replaceSlashes(path);
-        System.out.println(String.format("Check against %s", replaceSlashes("https:\\\\")));
+        System.out.println(String.format("Check %s against %s", path, replaceSlashes("https:\\\\")));
         if(path.contains(replaceSlashes("https:\\\\")))
                 path = path.replace("https:\\\\","");
         if(path.contains(replaceSlashes("http:\\\\")))
