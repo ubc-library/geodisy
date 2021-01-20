b1fa107777c564b4e7cde18e8369d15522a78261
Paul Dante <paul.dante@ubc.ca>
2021-01-19 18:55:19 -0800

    What call is shp2postgres making?

--- Geodisy/Geodisy/src/main/java/GeoServer/PostGIS.java
+++ Geodisy/Geodisy/src/main/java/GeoServer/PostGIS.java
@@ -26,6 +26,7 @@ public class PostGIS {
 
         String call = SHP_2_PGSQL + folderized(djo.getSimpleFieldVal(PERSISTENT_ID)) + "/" + fileName + " " + POSTGRES_SCHEMA + geoserverLabel + PSQL_CALL + VECTOR_DB + POSTGIS_USER_CALL;
         call = GeodisyStrings.replaceSlashes(call);
+        System.out.println(call);
         ProcessBuilder processBuilder= new ProcessBuilder();
         processBuilder.command("/usr/bin/bash", "-c", call);
         Process p;
