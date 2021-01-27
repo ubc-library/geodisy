d1341a8f25f23e58c3bad0acd27f85d51def0ff7
Paul Dante <paul.dante@ubc.ca>
2021-01-27 14:38:03 -0800

    Updated prod harvestapi url

--- Geodisy/Geodisy/src/main/java/_Strings/ENDPOINT_STRINGS.java
+++ Geodisy/Geodisy/src/main/java/_Strings/ENDPOINT_STRINGS.java
@@ -16,10 +16,10 @@ public class ENDPOINT_STRINGS {
     public static final String WINDOWS_ROOT             = "D:\\Work\\Geodisy\\Geodisy\\";
     public static final String FRDR_VM_CENTOS_ROOT      = "/home/centos/geodisy/Geodisy/Geodisy/";
 
-    //public static String DEV_HARVESTER_BASE             = "https://dev3.frdr.ca/harvesterapi/";
-    public static String PROD_HARVESTER_BASE            = "http://prod-web-g1.frdr.ca/harvestapi/";
+    public static String DEV_HARVESTER_BASE             = "https://dev3.frdr.ca/harvesterapi/";
+    public static String PROD_HARVESTER_BASE            = "https://www.frdr-dfdr.ca/harvestapi/";
 
     //FOR testing on TEST
 
-    public static String DEV_HARVESTER_BASE             = "https://test.frdr.ca/harvestapi/";
+    //public static String DEV_HARVESTER_BASE             = "https://test.frdr.ca/harvestapi/";
 }
