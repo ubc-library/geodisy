64b12effe8e95a39e57ea2bc14bd6257cb86c552
Paul Dante <paul.dante@ubc.ca>
2021-05-20 11:21:28 -0700

    Fixing encoding error fix

--- Geodisy/Geodisy/src/main/java/GeoServer/PostGIS.java
+++ Geodisy/Geodisy/src/main/java/GeoServer/PostGIS.java
@@ -38,7 +38,7 @@ public class PostGIS {
         try {
             results = pc.runProcess(call,10,TimeUnit.SECONDS,logger);
             System.out.println("Result: " + results);
-            if(!results.contains("Unable to convert data value to \'UTF-8\'")) {
+            if(!results.contains("Unable to convert data value to UTF-8")) {
                 System.out.println("Good convert: " + results);
                 return true;
             } else {
